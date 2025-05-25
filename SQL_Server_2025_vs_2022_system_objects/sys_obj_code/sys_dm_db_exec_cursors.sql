962b3f4292a91fbf6190890825bb2a0142e10871
Vlad Drumea <48413726+VladDBA@users.noreply.github.com>
2025-05-25 11:52:29 +0300

    re-upload

--- SQL_Server_2025_vs_2022_system_objects/sys_obj_code/sys_dm_db_exec_cursors.sql
+++
@@ -1,8 +0,0 @@
-SET ANSI_NULLS ON;
-SET QUOTED_IDENTIFIER ON;
-CREATE FUNCTION sys.dm_db_exec_cursors (@spid int)
-RETURNS table
-AS
-	RETURN SELECT *
-	FROM OpenRowSet(TABLE DM_DB_EXEC_CURSORS, @spid)
-
